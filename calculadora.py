237c0e180490eb2d52853da14d08d70a401ce313
Jeronimo Ferrer <>
2022-09-14 13:36:51 +0000

    Initial commit

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+## suma, resta, multiplicacion, division.__doc__
+
+valor1 = 8
+valor2 = 12
+
+resultado = valor1+valor2
+print(resultado)
+
+resultado2 = valor1 - valor2
+print(resultado2)
+
+resultado3 = valor1 * valor2
+print(resultado3)
+
+resultado4 = valor1 / valor2
+print(resultado4)
---
+++ main.py
@@ -0,0 +1,7 @@
+# Extraer un substring con python
+
+frase = "Goku es mejor que Vegeta"
+inicial = 0
+final = 4
+palabra = frase[inicial:final]
+print(palabra)
